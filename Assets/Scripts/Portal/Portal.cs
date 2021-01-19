833abf66c4c8a706b0533f9365dfd73975a15c5d
Ruenbaker <novaxxl2000@gmail.com>
2021-01-19 15:58:16 +0100

    Update Portal.cs

--- Assets/Scripts/Portal/Portal.cs
+++ Assets/Scripts/Portal/Portal.cs
@@ -191,7 +191,7 @@ public class Portal : MonoBehaviour {
             if (viewTexture != null) {
                 viewTexture.Release ();
             }
-            viewTexture = new RenderTexture (Screen.width, Screen.height, 16);
+            viewTexture = new RenderTexture (Screen.width, Screen.height, 24);
             // Render the view from the portal camera to the view texture
             portalCam.targetTexture = viewTexture;
             // Display the view texture on the screen of the linked portal
